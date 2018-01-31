c24297cda240efa40aca292e7d0657d7432d1eba
Tom Jackman <tom.dls@hotmail.com>
2018-01-31 22:41:09 +0000

    add docs tags

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -36,43 +36,63 @@ export class KeycloakService {
           });
       });
     }
-    // end::init[]
+  // end::init[]
+
+  // tag::getToken[]
   /**
   * Get Access Token
   */
   getToken(): string {
   return KeycloakService.auth.authz.token;
   }
+  // end::getToken[]
+
+  // tag::logout[]
   /**
   * Redirect to logout
   */
   logout(): void {
     KeycloakService.auth.authz.logout();
   }
+  // end::logout[]
+
+  // tag::login[]
   /**
    * Redirect to Login
    */
   login(): void {
     KeycloakService.auth.authz.login();
   }
+  // end::login[]
+
+  // tag::clearToken[]
   /**
    * Clears Authentication State
    */
   clearToken(): void {
     KeycloakService.auth.authz.clearToken();
   }
+  // end::clearToken[]
+
+  // tag::getRealmRoles[]
   /**
    * Return the users realm level roles
    */
   getRealmRoles(): void {
     return KeycloakService.auth.authz.realmAccess.roles;
   }
+  // end::getRealmRoles[]
+
+  // tag::hasRealmRole[]
   /**
    * Check if the user has a specified realm role
    */
   hasRealmRole(role: String): boolean {
     return KeycloakService.auth.authz.hasRealmRole(role);
   }
+  // end::hasRealmRole[]
+
+  // tag::getConfiguration[]
   /**
    * Get Server/Open ID Connect specific server info
    */
@@ -88,12 +108,18 @@ export class KeycloakService {
       "timeSkew": KeycloakService.auth.authz.timeSkew ? KeycloakService.auth.authz.timeSkew : notAvailable
     };
   }
+  // end::getConfiguration[]
+
+  // tag::accountManagement[]
   /**
    * Redirects to the Account Management Console
    */
   accountManagement(): void {
     KeycloakService.auth.authz.accountManagement();
   }
+  // end::accountManagement[]
+
+  // tag::loadUserProfile[]
   /**
    * Get the users profile
    */
@@ -107,17 +133,14 @@ export class KeycloakService {
       });
     });
   }
-  /**
-   * Check if the user has a given role
-   * @param role The role to check if the user posesses
-   */
-  viewGuard(role: string): boolean {
-        return true;
-  }
+  // end::loadUserProfile[]
+
+  // tag::isAuthenticated[]
   /**
    * Check if the user is authenticated
    */
   isAuthenticated(): boolean {
       return KeycloakService.auth.authz.authenticated;
   }
+  // end::isAuthenticated[]
 }
